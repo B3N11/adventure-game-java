d3d7f7bfe1fb609fc3f8ca26fcd452c2f2d5baf8
Benedek Kincses <benedek.kincses@gmail.com>
2023-10-16 12:32:50 +0200

    Init ActionButtonHandler

--- project/src/ui/handlers/UIHandler.java
+++ project/src/ui/handlers/UIHandler.java
@@ -1,6 +1,18 @@
 package ui.handlers;
 
+import uilogic.GridButtonHandler;
+
 public class UIHandler {
+
+    private GridButtonHandler gridButtonHandler;
+
+    public UIHandler(){
+        initUIHandler();
+    }
+
+    private void initUIHandler(){
+        gridButtonHandler = new GridButtonHandler();
+    }
     
     public void start(){
         
